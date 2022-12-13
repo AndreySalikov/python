dc98882403dd92fa882479bbb02e5cf7ef5cac79
Andrey Salikov <79645878868@ya.ru>
2022-12-13 19:33:25 +0300

    first commit

---
+++ Lections/HelloPython/hello.py
@@ -0,0 +1,329 @@
+
+
+# value = None
+# print(type(value))
+# a = 123
+# b = 1.23
+# print(a)
+# print(b)
+# value = 12334
+# print(value)
+# print(type(a))
+# print(type(b))
+# print(type(value))
+# s = '"Hello \n \'world'
+# print(s)
+# print(a, b, s)
+# print(a, '-',b, '-', s)
+# print('{1} - {2} - {0}'.format(a, b, s))
+# print(f'{a} - {b} - {s}')
+
+# f = True
+# print(f)
+
+# list = ['1','2','3']
+# col = ['hello', 1, 2, 3.14, True]
+# print(list)
+# print(col)
+
+# print() – отвечает за вывод данных
+# input() – отвечает за ввод данных
+
+# print('Enter a')
+# a = int(input())
+# print('Enter b')
+# b = float(input())
+# print(a,' + ', b, ' = ', a + b)
+# print('{} {}'.format(a,b))
+# print(f'{a} {b}')
+
+# a = int(input('Введите \nа: '))
+# b = int(input('Введите \nb: '))5
+# c = a + b
+# print('{} + {} = {}'.format(a, b, c))
+
+# Арифметические операции
+# +, -, * , /, %, //,**
+# Приоритет операций
+# **, ⊕, ⊖, *, /, //, %, +, -
+# ( ) Скобки меняют приоритет
+
+# a = 8.123
+# b = 3 
+# c = round(a + b, 5)
+# print(c)
+# print(a ** b)
+# print(a % b)
+# print(round(a // b))
+# exp1 = 2**3 - 10 % 5 + 2*3
+# exp2 = 2**3 - 10 / 5 + 2*3
+# print(exp1) # 14.0 или 14
+# print(exp2) # 12.0 или 12
+# exp1 = 2**3 - 10 % 5 + 2*3
+# exp2 = (2**3) - (10 / 5) + (2*3)
+# print(exp1) # 14.0 или 14
+# print(exp2) # 12.0 или 12
+
+# Сокращённые операции и операции присваивания
+# iter = 2
+# iter += 3 # iter = iter + 3
+# iter -= 4 # iter = iter - 4
+# iter *= 5 # iter = iter * 5
+# iter /= 5 # iter = iter / 5
+# iter //= 5 # iter = iter // 5
+# iter %= 5 # iter = iter % 5
+# iter **= 5 # iter = iter ** 5
+
+# a, b, c = 1, 2, 3
+# # a: 1 b: 2 c: 3
+# print('a: {} b: {} c: {}'.format(a, b, c))
+# range(*(1,5,2))
+
+# Логические операции
+
+# >, >=, <, <=, ==, !=
+# not, and, or – не путать с &, |, ^
+# Кое-что ещё: is, is not, in, not in
+
+# a = 1 < 4 and 5 > 2
+# b = 1 == 2
+# c = 1 != 2
+# print( a, b, c)
+
+# a = 'qwe'
+# b = 'qwe'
+# print(a == b)
+
+# a = [1,2]
+# b = [1,2]
+# print(a == b)
+
+# a = 1 < 3 < 5 < 10
+# print(a)
+
+# func = 1
+# T = 4
+# x = 2
+
+# print(func<T>(x))
+
+# f = 1 > 2 or 4 < 6 # Дизъюнкция двух высказываний называется высказывание тогда и только тогда, когда олдно из высказованийистино
+# print (f)
+
+# f = [1, 2, 3, 4]
+# is_odd = not f[0] % 2
+# print(is_odd)
+
+# Управляющие конструкции:
+# if, if-else
+
+# if condition:
+#  # operator 1
+#  # operator 2
+#  # ...
+#  # operator n
+# else:
+#  # operator n + 1
+#  # operator n + 2
+#  # ...
+#  # operator n + m
+
+# username = input('Введите имя: ')
+# if(username == 'Маша'):
+#   print('Ура, это же МАША!');
+# else:
+#   print('Привет, ', username);
+
+# if condition1:
+#  # operator
+# elif condition2:
+#  # operator
+# elif condition3:
+#  # operator
+# else:
+#  # operator
+
+# username = input('Введите имя: ')
+# if username == 'Маша':
+#     print('Ура, это же МАША!')
+# elif username == 'Марина':
+#     print('Я так ждала Вас, Марина!')
+# elif username == 'Ильнар':
+#     print('Ильнар - топ)')
+# else:
+#     print('Привет, ', username)
+
+# a = int(input('Enter a: '))
+# b = int(input('Enter b: '))
+# if a > b:
+#     print(a)
+# else:
+#     print(b)
+
+# Управляющие конструкции:
+# while
+
+# while condition:
+#  # operator 1
+#  # operator 2
+#  # . . .
+#  # operator n
+
+# original = 23
+# inverted = 0
+# while original != 0:
+#  inverted = inverted * 10 + (original % 10)
+#  original //= 10
+# print(inverted)
+
+# Управляющие конструкции: while-else
+# while condition:
+#  # operator 1
+#  # operator 2
+#  # . . .
+#  # operator n
+# else:
+#  # operator n + 1
+#  # operator n + 2
+#  # . . .
+#  # operator n + m
+
+# original = 23
+# inverted = 0
+# while original != 0:
+#     inverted = inverted * 10 + (original % 10)
+#     original //= 10
+# else:
+#     print('Пожалуй')
+#     print('хватит )')
+# print(inverted)
+# # Пожалуй
+# # хватит )
+# # 32
+
+# Управляющая конструкция 
+# for
+
+# for i in 1,2,3,4,5:
+#     print(i**2)
+
+# list = [1,2,3,4,5]
+# for i in list:
+#     print(i**2)
+
+
+
+# Знакомьтесь – range
+# r = range(5) # range(0, 5)
+# r = range(2, 5) # range(2, 5)
+# r = range(-5, 0) # range(-5, 0)
+# r = range(1, 10, 2) # range(1, 10, 2)
+# r = range(100, 0, -20) # range(100, 0, -20)
+
+# r = range(100, 0, -20) # range(100, 0, -20)
+# for i in r:
+#  print(i)
+# # 100 80 60 40 20
+# for i in range(5):
+#  print(i)
+# # 0 1 2 3 4
+
+# line = ""
+# for i in range(5):
+#     line = ""
+#     for j in range(5):
+#         line += "*"
+#     print(line)
+
+# r = range(10)
+# for i in r:
+#     print(i)
+
+# for i in range(10):
+#     print(i)
+
+# for i in range(5,15,5):
+#     print(i)
+
+# for i in 'qwe - rty':
+#     print(i)    
+
+
+# Немного о строках
+
+# text = 'съешь ещё этих мягких французских булок'
+# print(len(text)) # 39
+# print('ещё' in text) # True
+# print(text.isdigit()) # False
+# print(text.islower()) # True
+# print(text.replace('ещё','ЕЩЁ')) #
+# for c in text:
+#     print(c)
+
+
+# text = 'съешь ещё этих мягких французских булок'
+# print(text[0]) # c
+# print(text[1]) # ъ
+# print(text[len(text)-1]) # к
+# print(text[-5]) # б
+# print(text[:]) # print(text)
+# print(text[:2]) # съ
+# print(text[len(text)-2:]) # ок
+# print(text[2:9]) # ешь ещё
+# print(text[6:-18]) # ещё этих мягких
+# print(text[6:22]) # ещё этих мягких
+# print(text[0:len(text):6]) # сеикакл
+# print(text[::6]) # сеикакл
+# text = text[2:9] + text[-5] + text[:2] # ...
+# print(text)
+
+# Списки: введение
+
+# numbers = [1, 2, 3, 4, 5]
+# print(numbers) # [1, 2, 3, 4, 5]
+# numbers = list(range(1, 6))
+# print(numbers) # [1, 2, 3, 4, 5]
+# numbers[0] = 10
+# print(numbers) # [10, 2, 3, 4, 5]
+# for i in numbers:
+#     i *= 2
+#     print(i) # [20, 4, 6, 8, 10]
+# print(numbers) # [10, 2, 3, 4, 5]
+
+# colors = ['red', 'green', 'blue']
+# for e in colors:
+#     print(e) # red green blue
+# for e in colors:
+#     print(e*2) # redred greengreen blueblue
+# colors.append('gray') # добавить в конец
+# print(colors == ['red', 'green', 'blue', 'gray']) # True
+# colors.remove('red') #del colors[0] # удалить элемент
+
+# Функции
+
+# def function_name(x):
+# # body line 1
+# # . . .
+# # body line n
+#  # optional return
+
+# def f(x):
+#     return x**2
+
+# print(f(2))
+
+
+def f(x):
+    if x == 1:
+        return 'Целое'
+    elif x == 2.3:
+        return 23
+    else:
+        return
+
+print(f(1)) # Целое
+print(f(2.3)) # 23
+print(f(28)) # None
+print(type(f(1))) # str
+print(type(f(2.3))) # int
+print(type(f(28))) # NoneType
