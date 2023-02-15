2a6aeed4bfb2a4984d36190908154824bbb44a3f
Andrey Salikov <79645878868@ya.ru>
2023-02-15 19:19:53 +0300

    added new exampels

--- Lections/HelloPython/hello.py
+++ Lections/HelloPython/hello.py
@@ -1,22 +1,22 @@
 
 
-# value = None
-# print(type(value))
-# a = 123
-# b = 1.23
-# print(a)
-# print(b)
-# value = 12334
-# print(value)
-# print(type(a))
-# print(type(b))
-# print(type(value))
-# s = '"Hello \n \'world'
-# print(s)
-# print(a, b, s)
-# print(a, '-',b, '-', s)
-# print('{1} - {2} - {0}'.format(a, b, s))
-# print(f'{a} - {b} - {s}')
+value = None
+print(type(value))
+a = 123
+b = 1.23
+print(a)
+print(b)
+value = 12334
+print(value)
+print(type(a))
+print(type(b))
+print(type(value))
+s = '"Hello \n \'world'
+print(s)
+print(a, b, s)
+print(a, '-',b, '-', s)
+print('{1} - {2} - {0}'.format(a, b, s))
+print(f'{a} - {b} - {s}')
 
 # f = True
 # print(f)
@@ -29,13 +29,13 @@
 # print() – отвечает за вывод данных
 # input() – отвечает за ввод данных
 
-# print('Enter a')
-# a = int(input())
-# print('Enter b')
-# b = float(input())
-# print(a,' + ', b, ' = ', a + b)
-# print('{} {}'.format(a,b))
-# print(f'{a} {b}')
+print('Enter a')
+a = int(input())
+print('Enter b')
+b = float(input())
+print(a,' + ', b, ' = ', a + b)
+print('{} {}'.format(a,b))
+print(f'{a} {b}')
 
 # a = int(input('Введите \nа: '))
 # b = int(input('Введите \nb: '))5
@@ -214,90 +214,90 @@
 
 
 # Знакомьтесь – range
-# r = range(5) # range(0, 5)
-# r = range(2, 5) # range(2, 5)
-# r = range(-5, 0) # range(-5, 0)
-# r = range(1, 10, 2) # range(1, 10, 2)
-# r = range(100, 0, -20) # range(100, 0, -20)
+r = range(5) # range(0, 5)
+r = range(2, 5) # range(2, 5)
+r = range(-5, 0) # range(-5, 0)
+r = range(1, 10, 2) # range(1, 10, 2)
+r = range(100, 0, -20) # range(100, 0, -20)
 
-# r = range(100, 0, -20) # range(100, 0, -20)
-# for i in r:
-#  print(i)
-# # 100 80 60 40 20
-# for i in range(5):
-#  print(i)
-# # 0 1 2 3 4
+r = range(100, 0, -20) # range(100, 0, -20)
+for i in r:
+ print(i)
+# 100 80 60 40 20
+for i in range(5):
+ print(i)
+# 0 1 2 3 4
 
-# line = ""
-# for i in range(5):
-#     line = ""
-#     for j in range(5):
-#         line += "*"
-#     print(line)
+line = ""
+for i in range(5):
+    line = ""
+    for j in range(5):
+        line += "*"
+    print(line)
 
-# r = range(10)
-# for i in r:
-#     print(i)
+r = range(10)
+for i in r:
+    print(i)
 
-# for i in range(10):
-#     print(i)
+for i in range(10):
+    print(i)
 
-# for i in range(5,15,5):
-#     print(i)
+for i in range(5,15,5):
+    print(i)
 
-# for i in 'qwe - rty':
-#     print(i)    
+for i in 'qwe - rty':
+    print(i)    
 
 
 # Немного о строках
 
-# text = 'съешь ещё этих мягких французских булок'
-# print(len(text)) # 39
-# print('ещё' in text) # True
-# print(text.isdigit()) # False
-# print(text.islower()) # True
-# print(text.replace('ещё','ЕЩЁ')) #
-# for c in text:
-#     print(c)
+text = 'съешь ещё этих мягких французских булок'
+print(len(text)) # 39
+print('ещё' in text) # True
+print(text.isdigit()) # False
+print(text.islower()) # True
+print(text.replace('ещё','ЕЩЁ')) #
+for c in text:
+    print(c)
 
 
-# text = 'съешь ещё этих мягких французских булок'
-# print(text[0]) # c
-# print(text[1]) # ъ
-# print(text[len(text)-1]) # к
-# print(text[-5]) # б
-# print(text[:]) # print(text)
-# print(text[:2]) # съ
-# print(text[len(text)-2:]) # ок
-# print(text[2:9]) # ешь ещё
-# print(text[6:-18]) # ещё этих мягких
-# print(text[6:22]) # ещё этих мягких
-# print(text[0:len(text):6]) # сеикакл
-# print(text[::6]) # сеикакл
-# text = text[2:9] + text[-5] + text[:2] # ...
-# print(text)
+text = 'съешь ещё этих мягких французских булок'
+print(text[0]) # c
+print(text[1]) # ъ
+print(text[len(text)-1]) # к
+print(text[-5]) # б
+print(text[:]) # print(text)
+print(text[:2]) # съ
+print(text[len(text)-2:]) # ок
+print(text[2:9]) # ешь ещё
+print(text[6:-18]) # ещё этих мягких
+print(text[6:22]) # ещё этих мягких
+print(text[0:len(text):6]) # сеикакл
+print(text[::6]) # сеикакл
+text = text[2:9] + text[-5] + text[:2] # ...
+print(text)
 
 # Списки: введение
 
-# numbers = [1, 2, 3, 4, 5]
-# print(numbers) # [1, 2, 3, 4, 5]
-# numbers = list(range(1, 6))
-# print(numbers) # [1, 2, 3, 4, 5]
-# numbers[0] = 10
-# print(numbers) # [10, 2, 3, 4, 5]
-# for i in numbers:
-#     i *= 2
-#     print(i) # [20, 4, 6, 8, 10]
-# print(numbers) # [10, 2, 3, 4, 5]
+numbers = [1, 2, 3, 4, 5]
+print(numbers) # [1, 2, 3, 4, 5]
+numbers = list(range(1, 6))
+print(numbers) # [1, 2, 3, 4, 5]
+numbers[0] = 10
+print(numbers) # [10, 2, 3, 4, 5]
+for i in numbers:
+    i *= 2
+    print(i) # [20, 4, 6, 8, 10]
+print(numbers) # [10, 2, 3, 4, 5]
 
-# colors = ['red', 'green', 'blue']
-# for e in colors:
-#     print(e) # red green blue
-# for e in colors:
-#     print(e*2) # redred greengreen blueblue
-# colors.append('gray') # добавить в конец
-# print(colors == ['red', 'green', 'blue', 'gray']) # True
-# colors.remove('red') #del colors[0] # удалить элемент
+colors = ['red', 'green', 'blue']
+for e in colors:
+    print(e) # red green blue
+for e in colors:
+    print(e*2) # redred greengreen blueblue
+colors.append('gray') # добавить в конец
+print(colors == ['red', 'green', 'blue', 'gray']) # True
+colors.remove('red') #del colors[0] # удалить элемент
 
 # Функции
 
@@ -307,10 +307,10 @@
 # # body line n
 #  # optional return
 
-# def f(x):
-#     return x**2
+def f(x):
+    return x**2
 
-# print(f(2))
+print(f(2))
 
 
 def f(x):
